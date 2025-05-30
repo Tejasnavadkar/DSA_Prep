3b6d333894ae9e9393a5accf43e37550857fbc1d
Tejasnavadkar <tejas.navadkar@gmail.com>
2025-05-30 23:04:07 +0530

    Q - Maximum Subarray & Majority element / [Moore's voting algorithm]

--- Advance-Array-part-6/App.js
+++ Advance-Array-part-6/App.js
@@ -181,13 +181,13 @@ while(i<arr1.length && j<arr2.length){
     }
 }
 
-while(i<arr1.length){
+while(i<arr1.length){  // if j exhauste the put all remaining arr1 elements to temp 
     temp[k] = arr1[i]
     k++
     i++
 }
 
-while(j<arr2.length){
+while(j<arr2.length){  // if i exhauste the put all remaining arr2 elements to temp
     temp[k] = arr2[j]
     k++
     j++
@@ -200,3 +200,135 @@ console.log(temp)
 //   1, 2, 3, 4,
 //   5, 6, 8
 // ]
+
+
+// merge sorted array [leetcode]
+
+var merge = function(nums1, m, nums2, n) {
+   let k = m
+    
+    for(let i =0;i<n;i++){
+        nums1[k] = nums2[i]
+        k++
+    }
+    nums1.sort((a, b) => a - b); // here dont use nums1.sort() gives error this only use when there is strings for numbers use nums1.sort((a, b) => a - b)
+};
+
+// nums1 = [1,2,3,0,0,0] nums2 = [2,5,6]
+// o/p [1,2,2,3,5,6]
+
+
+// Q-6] best time to buy and sell stocks
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+prices = [7,1,5,3,6,4] //o/p 5
+
+let min = prices[0]
+let maxProfit = 0
+let count = 0
+
+for (let i = 0;i<prices.length;i++){
+    
+    if(prices[i] < min){
+            min = prices[i]
+    }
+    let profit = prices[i] - min
+       if(profit > maxProfit){
+           maxProfit = profit
+       }
+}
+console.log(maxProfit)
+
+
+
+//sort the color //Approach : Dutch National Flag algorithm
+let nums = [2,0,2,1,1,0]
+//Output: [0,0,1,1,2,2]
+
+let j = 0       // here we set 3 pointers
+let k = nums.length - 1
+let i = 0
+while(i <= k){
+    if(nums[i] === 0){
+        let temp = nums[i]
+        nums[i] = nums[j]
+        nums[j] = temp
+        j++
+        i++
+    }else if(nums[i]===2){
+        let temp = nums[i]
+        nums[i] = nums[k]
+        nums[k] = temp
+        k--
+    }
+    else i++
+    
+    
+}
+console.log(nums)
+
+
+//Q-7] Maximum Subarray/kadanes algorithm
+
+let nums = [-2,1,-3,4,-1,2,1,-5,4] //o/p 6
+
+let sum = 0
+let maxSum = -Infinity // -Infinity is the smallest number
+
+for(let i=0;i<nums.length;i++){ //pointer starts with 0
+    sum += nums[i]
+    if(sum>maxSum){
+        maxSum = sum
+    }
+    if(sum<0)sum = 0
+}
+
+console.log(maxSum)
+
+// Q-8] Majority element / [Moore's voting algorithm]
+
+
+// brute force
+let nums = [3,1,2,2,3,3,3,3]
+
+for(let i=0;i<nums.length;i++){
+    let count = 0
+    for(let j=0;j<nums.length;j++){
+        if(nums[j] === nums[i]){
+            count++
+        }
+    }
+    if(count>nums.length/2){
+        console.log(nums[i])
+        break;
+    }
+}
+
+
+// optimised [Moore's voting algorithm]
+let nums = [2,2,1,1,1,2,2]
+let j = 1
+let ans = nums[0]
+let count = 0
+for(let i = 0;i<nums.length;i++){
+    if(count===0)ans=nums[i]
+    if(nums[i] === ans){
+        count++
+    }else count--
+}
+
+console.log(ans)
+
+// o/p 2
+
+
+// Q-9] Trapping rain problem
+
+
+
